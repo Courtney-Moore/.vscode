52c79707020bdf233122ff0128c65f82c1f6f1c2
Courtney <74709010+LassieofPython@users.noreply.github.com>
2022-02-02 11:14:55 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+var neilLikes= document.querySelector('#neillikes');
+console.log(neilLikes);
+
+function neilincreaseLikes(){
+    neilLikes.innerText++
+}
+
+var nicholelikes= document.querySelector('#nicholelikes');
+console.log(nicholelikes);
+
+function nicholeincreaseLikes(){
+    nicholeLikes.innerText++
+}
+var jimLikes= document.querySelector('#jimLikes');
+
+function jimincreaseLikes(){
+    jimLikes.innerText++
+}
